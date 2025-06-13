32c9c0d85128fa300ecf804b591efa2486bc5358
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-06-13 21:59:54 +0300

    required true

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -3,36 +3,39 @@ import {Schema, model} from 'mongoose'
 const movieSchema = new Schema({
     title: {
         type: String,
+        required: true
     }, 
     category: {
         type: String,
-       
+        required: true
     },
     genre: {
         type: String,
-       
+        required: true
        
     },
     director: {
         type: String,
-       
+        required: true
     },
     year: {
         type: Number,
-       
-       
+        required: true,
+        min: 1970,
+        max: new Date().getFullYear() + 5       
     },
     imageUrl: {
         type: String,
-        
+        required: true
     },
     rating: {
         type: Number,
-       
+        required: true
     },
     description: {
         type: String,
-        
+        required: true,
+        maxLengthL [100, 'Description is too long']
     }
 });
 
